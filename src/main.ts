7fa546dfe76a9c8cb72594602927405880ad0b28
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2021-11-20 17:21:58 +0700

    update windicss

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'virtual:windi-base.css'
+import 'virtual:windi-components.css'
+import 'virtual:windi-utilities.css'
+
+
 if (environment.production) {
   enableProdMode();
 }
---
+++ windi.config.ts
@@ -0,0 +1,3 @@
+export default {
+  attributify: true,
+}
