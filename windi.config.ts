932c4c229c8e01f01cedf5c970cca4f2410a3892
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2021-11-21 23:18:50 +0700

    ีupdate

--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,22 @@
-export default {
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
   attributify: {
     prefix: 'w:',
   },
-}
+  shortcuts: {
+    'btn': 'py-2 px-4 font-semibold rounded-lg shadow-md',
+    'btn-green': 'text-white bg-green-500 hover:bg-green-700',
+  },
+  extract: {
+    // accepts globs and file paths relative to project root
+    include: [
+      'index.html',
+      'src/**/*.{html,js,ts}',
+    ],
+    exclude: [
+      'node_modules/**/*',
+      '.git/**/*',
+    ],
+  },
+})
