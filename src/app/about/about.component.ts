9627efb50570510b4ac9f178c5a8b6c77ca4e3e9
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2021-11-28 16:51:38 +0700

    update style mode dark

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  initLight = true
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  click1() {
+    if (this.initLight) {
+      this.initLight = false
+      document.documentElement.classList.add('dark')
+      document.body.style.backgroundColor = "black";
+    }
+  }
+  click2() {
+    if (!this.initLight) {
+      this.initLight = true
+      document.documentElement.classList.remove('dark')
+      document.body.style.backgroundColor = "white";
+    }
+  }
 }
--- windi.config.ts
+++ windi.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
+  darkMode: 'class',
+  theme: {
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
+  },
+  alias:{
+    bird: 'py-2 px-4 font-semibold rounded-lg text-white bg-green-500 hover:bg-green-700'
+  },
   attributify: {
     prefix: 'w:',
   },
   shortcuts: {
-    'btn': 'py-2 px-4 font-semibold rounded-lg shadow-md',
+    'btn': {
+      'color': 'white',
+      '@apply': 'cursor-pointer py-2 px-4 font-semibold rounded-lg bg-gray-400',
+      '&:hover': {
+        '@apply': 'bg-green-700',
+        'color': 'white',
+      },
+    },
     'btn-green': 'text-white bg-green-500 hover:bg-green-700',
   },
   extract: {
